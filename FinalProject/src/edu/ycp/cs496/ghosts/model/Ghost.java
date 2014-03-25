da17034dde79cc5bfaa427a8150dcb52b2426748
Shane Bonner <sbonner1@ycp.edu>
2014-03-25 14:52:02 -0400

    initial checkin

---
+++ FinalProject/src/edu/ycp/cs496/ghosts/model/Ghost.java
@@ -0,0 +1,5 @@
+package edu.ycp.cs496.ghosts.model;
+
+public class Ghost {
+
+}
---
+++ FinalProjectUnitTests/src/edu/ycp/cs496/ghosts/model/GhostTest.java
@@ -0,0 +1,19 @@
+package edu.ycp.cs496.ghosts.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GhostTest {
+	private Ghost ghost;
+	
+	@Before
+	public void setUp() {
+		ghost = new Ghost();
+	}
+	
+	@Test
+	public void testGhost() {
+		// TODO: test
+	}
+}
