f106337c0b0af5c558184910ea58b5854d6dbcac
jcoady9 <droidmaker42@gmail.com>
2014-03-25 18:24:53 -0400

    added a controller for trophy information

---
+++ CS496_Project_App/src/ycp/edu/cs496project/mobileApp/servletControllers/TrophyController.java
@@ -0,0 +1,5 @@
+package ycp.edu.cs496project.mobileApp.servletControllers;
+
+public class TrophyController {
+	//TODO: implement a method(s) to retrieve trophy information from the database (if trophy info is going to be stored on db)
+}
