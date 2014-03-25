fcb049d328e0ddb89e79d2bb96e6ec7b659328e6
jcoady9 <droidmaker42@gmail.com>
2014-03-25 19:03:33 -0400

    added stuff to app project

--- CS496_Project_App/src/ycp/edu/cs496project/mobileApp/MainActivity.java
+++ CS496_Project_App/src/ycp/edu/cs496project/mobileApp/MainActivity.java
@@ -3,13 +3,10 @@ package ycp.edu.cs496project.mobileApp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
-	
-	//This is a test to see if the github actually works
-	
-	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +19,26 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/**
+	 * an onClick method to start the game
+	 * 
+	 * @param v a view
+	 */
+	public void onPlayClick(View v){
+		//a simple test to see if the button works
+		Toast.makeText(MainActivity.this, " play Button test.", Toast.LENGTH_SHORT).show();
+	}
+	
+	/**
+	 * an onClick method to see the game's highscores
+	 * 
+	 * @param v a view
+	 */
+	public void onScoreClick(View v){
+		//simple test to see button works
+		Toast.makeText(MainActivity.this, "score button test.", Toast.LENGTH_SHORT).show();
 	}
 
 }
--- CS496_Project_App/src/ycp/edu/cs496project/mobileApp/servletControllers/TrophyController.java
+++ CS496_Project_App/src/ycp/edu/cs496project/mobileApp/servletControllers/TrophyController.java
@@ -1,5 +1,11 @@
 package ycp.edu.cs496project.mobileApp.servletControllers;
 
+/**
+ * a controller to handle information about the player's trophies
+ * 
+ * @author josh coady
+ *
+ */
 public class TrophyController {
 	//TODO: implement a method(s) to retrieve trophy information from the database (if trophy info is going to be stored on db)
 }
