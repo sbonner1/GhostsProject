430bbcf5612049f408a87ebbc7036d9fb88a638d
Chris Flinchbaugh <cflinch2@ycp.edu>
2014-03-26 15:24:58 -0400

    Added data variables to the Model classes, implemented new Ghost classes, updated GhostTest

---
+++ FinalProject/src/edu/ycp/cs496/ghosts/model/Gameplay.java
@@ -0,0 +1,10 @@
+package edu.ycp.cs496.ghosts.model;
+
+public class Gameplay 
+{
+	float spawnRate;
+	float rareSpawnRate;
+	int highScore;
+	int currentScore;
+
+}
---
+++ FinalProject/src/edu/ycp/cs496/ghosts/model/RareGhost_001.java
@@ -0,0 +1,5 @@
+package edu.ycp.cs496.ghosts.model;
+
+public class RareGhost_001 {
+
+}
