e877db0e8e556019169e5a0d5dcb2fb7d97e1682
jcoady9 <droidmaker42@gmail.com>
2014-04-09 17:49:16 -0400

    added user model to the app project.

---
+++ CS496_Project_App/src/ycp/edu/cs496project/mobilApp/model/User.java
@@ -0,0 +1,52 @@
+package ycp.edu.cs496project.mobilApp.model;
+/**
+ * @author sbonner1
+ *
+ * * This class is the basic model construct for the user. 
+ */
+public class User {
+	private String userName;
+	private String passwordHash;
+	private String password;
+	
+	private int score;
+	
+	public User(String userName, String password) {
+		//access database to pull name/password info
+		score = 0;
+		
+	}
+	//getter and setter methods for this class
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserPassword(String password){
+		this.password = password;
+	}
+	
+	public String getUserPassword(){
+		return password;
+	}
+	
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+	
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+	
+	public void setUserScore(int score){
+		this.score = score;
+	}
+	
+	public int getUserScore(){
+		return score;
+	}
+}
