4104c51b3793ed0e2efa7adf266005b9a23a1149
jcoady9 <droidmaker42@gmail.com>
2014-04-27 16:50:55 -0400

    applied Chris's updates for showing current score and timer in game's UI on screen

--- CS496_Project_App/src/ycp/edu/cs496project/mobileApp/graphics/Panel.java
+++ CS496_Project_App/src/ycp/edu/cs496project/mobileApp/graphics/Panel.java
@@ -26,6 +26,8 @@ public class Panel extends SurfaceView implements Callback
 	//final private float holeRadius = 20.0f;
 	//final private float ballStopped = 0.1f;
 	private int tooManyGhosts;
+	private int score;
+	private double countdownTime;
 	
 	/* TODO 1: Add fields for: Sprite (for the ball object), 
 	 * a field for a thread object, 
@@ -48,8 +50,10 @@ public class Panel extends SurfaceView implements Callback
 		generater = new Random(System.currentTimeMillis());
 
 		// TODO 2: Initialize class fields
+			score = 0;
+			tooManyGhosts = 10;
+			countdownTime = 30.0;
 			
-			tooManyGhosts = 50;
 			//Register the class as the callback (using getHolder.addCallback(this);)
 			getHolder().addCallback(this);
 			
@@ -62,10 +66,12 @@ public class Panel extends SurfaceView implements Callback
 			
 			//Set the game flag to false.
 			gameOver = false;
+			
+			
 
 			//ADD CODE IN THE CONSTRUCTOR 2 PART E			
 			//Add one sprite to the list (for the first ghost) starting at (0,0) (or wherever you wish) and with a random velocity between -3 and 3. Don't forget to set the field for the number of sprites.
-			createGhost(GhostEnums.redGhost,0,0,3);
+			createGhost(GhostEnums.redGhost,0,this.getWidth(),3);
 	}
 	
 	/**
@@ -115,27 +121,11 @@ public class Panel extends SurfaceView implements Callback
 	{
 		if (gameOver != true)
 		{
-			  int nextGhost = getRandomNum(0, 1000);
-		      if (nextGhost > 950)
-		      {
-		    	  createGhost(GhostEnums.redGhost,0,0,3);
-		      }
-		      
-		      if (nextGhost > 925 && nextGhost < 950)
-		      {
-		    	  createGhost(GhostEnums.yellowGhost,0,0,3);
-		      }
-		      
-		      if (nextGhost > 900 && nextGhost < 925)
-		      {
-		    	  createGhost(GhostEnums.greenGhost,0,0,4);
-		      }
-		      
-		      
 		   synchronized (mSpriteList) 
 		   {
 		      for (Sprite sprite : mSpriteList) 
 		      {
+		    	 //show movement
 		         sprite.update(elapsedTime);
 		         if (checkGameEnd() == true)
 		         {
@@ -145,7 +135,28 @@ public class Panel extends SurfaceView implements Callback
 		   }
 		}
 	}
-
+	
+	/*
+	 * Creates a random number and based on the number spawns the appropriate entity
+	 */
+	public void tryToSpawn()
+	{
+		 int nextGhost = getRandomNum(0, 1000);
+	     if (nextGhost > 950)
+	     {
+	   	  createGhost(GhostEnums.redGhost,0,0,3);
+	     }
+	     
+	     if (nextGhost > 925 && nextGhost < 950)
+	     {
+	   	  createGhost(GhostEnums.yellowGhost,0,0,3);
+	     }
+	     
+	     if (nextGhost > 900 && nextGhost < 925)
+	     {
+	   	  createGhost(GhostEnums.greenGhost,0,0,4);
+	     }
+	}
 	/**
 	 * Creates a ghost based on the ghostEnum type and randomly sets the initial location and velocity based on the ranges passed in as parameters 
 	 * @param ghostEnum
@@ -200,19 +211,19 @@ public class Panel extends SurfaceView implements Callback
 		        sprite.doDraw(canvas);
 		    }
 			
-			
-//create a getter for gameover and call it from the view
-			//Display message if game over
-			if (gameOver == true)
+			if (checkGameEnd() == true)
 			{
-				//Toast.makeText(Panel, "You Win!", Toast.LENGTH_SHORT).show();
+				canvas.drawText("Game Over", 10, 30, mPaint);
 			}
+			canvas.drawText(getCountdownString(getCountdownTime()), 10, 10, mPaint);
+			canvas.drawText(getScoreString(getScore()), 100, 10, mPaint);
 		}
 	}
 	
 	/* TODO 7: Add code to the onTouchEvent() event handler to compute a change in velocity based on a user swipe, i.e. the further they swipe the more there will be a velocity change in that direction.*/
 	@Override
-	public boolean onTouchEvent(MotionEvent event) {
+	public boolean onTouchEvent(MotionEvent event) 
+	{
 	//public boolean onClickEvent (MotionEvent event){
 	    // Get touch event location
 	    float currentX = event.getX();
@@ -222,43 +233,76 @@ public class Panel extends SurfaceView implements Callback
 	    // Compute scale factor based on window size
 	    float scalingFactor = 5.0f / ((mWidth > mHeight) ? mWidth : mHeight);
 
-	    // Use move touch event
-	    switch (event.getAction()) {
-	     	//case MotionEvent.ACTION_MOVE:
-	    	case MotionEvent.ACTION_DOWN: 
-	            // Modify velocities according to movement
-	            //deltaX = currentX - previousX;
-	            //deltaY = currentY - previousY;
-	        		//Updated
-	        	//nextGhost = Min + (int)(Math.random() * ((Max - Min) + 1));
-	        		deltaX = getRandomNum(-3, 3);
-	        		deltaY = getRandomNum(-3, 3);
-	        		
-            //when there is a move action ALL of the sprites will move
-	            // Update ball velocities
-	        	synchronized(mSpriteList)
-	        	{
-	        		ArrayList<Sprite> toRemove = new ArrayList<Sprite>();
-	        		for (Sprite sprite : mSpriteList) 
-		    		{
-	
-//?						if (sprite.getGhostType() < 3)
-	        			{
+	    if (checkGameEnd() != true)
+	    {
+		    // Use move touch event
+		    switch (event.getAction()) 
+		    {
+		     	//case MotionEvent.ACTION_MOVE:
+		    	case MotionEvent.ACTION_DOWN: 
+		            // Modify velocities according to movement
+		            //deltaX = currentX - previousX;
+		            //deltaY = currentY - previousY;
+		        		//Updated
+		        	//nextGhost = Min + (int)(Math.random() * ((Max - Min) + 1));
+		        		deltaX = getRandomNum(-3, 3);
+		        		deltaY = getRandomNum(-3, 3);
+		        		
+	            //when there is a move action ALL of the sprites will move
+		            // Update ball velocities
+		        	synchronized(mSpriteList)
+		        	{
+		        		ArrayList<Sprite> toRemove = new ArrayList<Sprite>();
+		        		for (Sprite sprite : mSpriteList) 
+			    		{
 		        			//draw the ball(s) because they are sprites in the sprite list
-			    			if (Math.abs(distance(currentX, currentY, sprite.getCenterX(), sprite.getCenterY())) < sprite.getRadius()* 3)
+			    			if (Math.abs(distance(currentX, currentY, sprite.getCenterX(), sprite.getCenterY())) < sprite.getRadius()*2)
 			    			{
 			    				toRemove.add(sprite);
+			    				score++;
 			    				//sprite.changeVelocity(deltaX * scalingFactor, deltaY * scalingFactor);
 			    			}
-			    			//sprite.changeVelocity(deltaX * scalingFactor, deltaY * scalingFactor);
-	        			}
-		    		}
-	        		
-	        		for (Sprite sprite : toRemove) {
-	        			mSpriteList.remove(sprite);
-	        		}
-	        	}
+			    		}
+		        		
+		        		for (Sprite sprite : toRemove) {
+		        			mSpriteList.remove(sprite);
+		        		}
+		        	}
+	/*	        	
+		     	case MotionEvent.ACTION_MOVE:
+		           		deltaX = getRandomNum(-3, 3);
+		        		deltaY = getRandomNum(-3, 3);
+		        		
+	            //when there is a move action ALL of the sprites will move
+		            // Update ball velocities
+		        	synchronized(mSpriteList)
+		        	{
+		        		ArrayList<Sprite> toRemove = new ArrayList<Sprite>();
+		        		for (Sprite sprite : mSpriteList) 
+			    		{
+		
+	//?						if (sprite.getGhostType() < 3)
+		        			{
+			        			//draw the ball(s) because they are sprites in the sprite list
+				    			if (Math.abs(distance(currentX, currentY, sprite.getCenterX(), sprite.getCenterY())) < sprite.getRadius()* 3)
+				    			{
+				    				toRemove.add(sprite);
+				    				score++;
+				    				//sprite.changeVelocity(deltaX * scalingFactor, deltaY * scalingFactor);
+				    			}
+				    			//sprite.changeVelocity(deltaX * scalingFactor, deltaY * scalingFactor);
+		        			}
+			    		}
+		        		
+		        		for (Sprite sprite : toRemove) {
+		        			mSpriteList.remove(sprite);
+		        		}
+		        	}
+		    }
+	*/
+		    }
 	    }
+
 
 	    // Save current x, y
 	    previousX = currentX;
@@ -271,10 +315,31 @@ public class Panel extends SurfaceView implements Callback
 		return (float) Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
 	}
 	
-	private boolean checkGameEnd() 
+	public void updateCountdownTime(double value)
+	{
+		countdownTime -= value;
+	}
+	
+	public double getCountdownTime()
+	{
+		return countdownTime;
+	}
+	
+	private String getCountdownString(double countdownTime)
+	{
+		return String.valueOf(countdownTime);
+	}
+	
+	private String getScoreString(int score)
+	{
+		return String.valueOf(score);
+	}
+	
+	public boolean checkGameEnd() 
 	{
 		// TODO 8: Check if ball is within hole region
-		if (mSpriteList.size() > tooManyGhosts) 
+		//if (mSpriteList.size() > tooManyGhosts)
+		if (getCountdownTime() <= 0.0)
 		{
 			return true;
 		}
@@ -282,6 +347,10 @@ public class Panel extends SurfaceView implements Callback
 		return false;
 	}
 
+	public int getScore()
+	{
+		return score;
+	}
 	
 	
 }
