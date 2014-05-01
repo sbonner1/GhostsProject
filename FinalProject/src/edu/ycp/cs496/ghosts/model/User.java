226d4dfdaf08b61663e08d62c2ea16f3216bd255
sbonner1 <sbonner1@ycp.edu>
2014-05-01 11:05:49 -0400

    fixed the addUser so that the password doesn't need to be sent through a JSON document, and is instead pulled as a string from the user JSON document

--- FinalProject/src/edu/ycp/cs496/ghosts/model/User.java
+++ FinalProject/src/edu/ycp/cs496/ghosts/model/User.java
@@ -14,7 +14,7 @@ public class User implements Cloneable {
 	public User(String userName, String password) {
 		//access database to pull name/password info
 		this.userName = userName;
-		this.passwordHash = password;
+		this.password = password;
 		score = 0;
 		
 	}
@@ -36,13 +36,14 @@ public class User implements Cloneable {
 		return password;
 	}
 	
-	public void setPasswordHash(String passwordHash) {
+	/*public void setPasswordHash(String passwordHash) {
 		this.passwordHash = passwordHash;
 	}
 	
 	public String getPasswordHash() {
 		return passwordHash;
 	}
+	*/
 	
 	public void setUserScore(int score){
 		this.score = score;
