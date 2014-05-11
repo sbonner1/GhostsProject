d0bd3641bef915c74621186f3a561678cd9935a2
sbonner1 <sbonner1@ycp.edu>
2014-05-11 15:36:24 -0400

    retry commit

--- FinalProjectControllers/src/edu/ycp/cs496/ghosts/model/persist/DerbyDatabase.java
+++ FinalProjectControllers/src/edu/ycp/cs496/ghosts/model/persist/DerbyDatabase.java
@@ -42,7 +42,6 @@ public class DerbyDatabase implements IDatabase {
 
 			@Override
 			public User execute(Connection conn) throws SQLException {
-				// TODO Auto-generated method stub
 				PreparedStatement stmt = null;
 				ResultSet resultSet = null;
 				try {
