07e9066ec372c7fe11af17fb35ae5e708ec93831
jcoady9 <droidmaker42@gmail.com>
2014-05-11 13:59:25 -0400

    stuff

--- FinalProject/src/edu/ycp/cs496/ghosts/model/User.java
+++ FinalProject/src/edu/ycp/cs496/ghosts/model/User.java
@@ -1,27 +1,123 @@
 package edu.ycp.cs496.ghosts.model;
+
+//import ycp.edu.cs496project.mobileApp.model.User;
+
 /**
  * @author sbonner1
  *
  * * This class is the basic model construct for the user. 
  */
+
+//CLIENT SIDE
 public class User implements Cloneable {
 	private String userName;
-	//private String passwordHash;
+	private String passwordHash;
 	private String password;
-	private int id;
+	
 	private int score;
 	
 	public User() {
-		this.userName = "";
-		this.password = "";
+		
 	}
-
 	
 	public User(String userName, String password) {
 		//access database to pull name/password info
 		this.userName = userName;
 		this.password = password;
 		score = 0;
+		
+	}
+	//getter and setter methods for this class
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserPassword(String password){
+		this.password = password;
+	}
+	
+	public String getUserPassword(){
+		return password;
+	}
+	
+	/*
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+	
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+	*/
+	
+	public void setUserScore(int score){
+		this.score = score;
+	}
+	
+	public int getUserScore(){
+		return score;
+	}
+	
+	@Override
+	public User clone() {
+		try {
+			User dup = (User) super.clone();
+			return dup;
+		} catch (CloneNotSupportedException e) {
+			throw new IllegalStateException("This can't happen", e);
+		}
+	}
+}
+
+
+
+
+
+/**
+ * @author sbonner1
+ *
+ * * This class is the basic model construct for the user. 
+ */
+
+//SERVER SIDE
+
+/*
+public class User implements Cloneable {
+	private String userName;
+	//private String passwordHash;
+	private String password;
+	private int id;
+	private int score;
+	private int highScore;
+	private int redChain;
+	private int yellowChain;
+	private int greenChain;
+	
+	public User() {
+		this.userName = "";
+		this.password = "";
+		this.highScore = 0;
+		this.redChain = 0;
+		this.yellowChain = 0;
+		this.greenChain = 0;
+	}
+
+	
+	public User(String userName, String password){//, int highScore, int redChain, int yellowChain, int greenChain) {
+		//access database to pull name/password info
+		this.userName = userName;
+		this.password = password;
+		
+		//this.highScore = 0;
+		//this.redChain = 0;
+		//this.yellowChain = 0;
+		//this.greenChain = 0;
+		
 	}
 	//getter and setter methods for this class
 	
@@ -55,15 +151,15 @@ public class User implements Cloneable {
 	public String getPasswordHash() {
 		return passwordHash;
 	}
-	*/
 	
-	public void setUserScore(int score){
-		this.score = score;
-	}
 	
-	public int getUserScore(){
-		return score;
-	}
+	//public void setUserScore(int score){
+	//	this.score = score;
+	//}
+	
+	//public int getUserScore(){
+	//	return score;
+	//}
 	
 	@Override
 	public User clone() {
@@ -75,3 +171,5 @@ public class User implements Cloneable {
 		}
 	}
 }
+
+*/
